ca2d74bce687d02190f4b7b89ebe55672e4a2f2d
adam <adam.lepkowski@hotmail.com>
2020-06-30 20:50:43 +0200

    Add rename method

--- rename_tool.py
+++ rename_tool.py
@@ -1,4 +1,16 @@
-class RenameTool:
+import os
 
-    def rename(self):
-        pass
+
+class RenameTool:
+    def __init__(self, matcher):
+        self.matcher = matcher
+
+    def rename(self, path, pattern, new_pattern):
+        if os.path.exists(path):
+            to_update = self.matcher.match(path, pattern)
+            updated = []
+            for file in to_update:
+                new = file.replace(pattern, new_pattern)
+                updated.append(new)
+                os.rename(dst=os.path.join(path, new), src=os.path.join(path, file))
+            return updated
--- test_rename_tool.py
+++ test_rename_tool.py
@@ -1,12 +1,30 @@
 from rename_tool import RenameTool
+from matcher import Matcher
 import pytest
 
 
 @pytest.fixture(name='rename')
-def fixture_rename():
-    rename_tool = RenameTool()
+def rename():
+    matcher = Matcher()
+    rename_tool = RenameTool(Matcher())
     return rename_tool
+
+
+@pytest.fixture(name='temp_files')
+def temp_files(tmp_path):
+    d = tmp_path
+    for i in range(10):
+        p = d / f'my_file{i}.txt'
+        p.write_text(f'{i}')
 
 
 def test_init(rename):
     assert rename
+
+
+def test_rename(rename, temp_files, tmp_path):
+    pattern = 'my_file'
+    new_pattern = 'test_file'
+    result = rename.rename(tmp_path, pattern, new_pattern)
+    expected = [f'{new_pattern}{i}.txt' for i in range(10)]
+    assert sorted(result) == sorted(expected)
