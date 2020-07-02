0874b9909c4afb8551f13598b1d6e83e91de9901
adam <adam.lepkowski@hotmail.com>
2020-07-02 12:56:24 +0200

    Add return_state

--- snapshot.py
+++ snapshot.py
@@ -1,8 +1,11 @@
 class Snapshot:
     def __init__(self):
         self.states = []
-        self._i = -1
+        self.state = -1
 
     def save_state(self, path, pattern, new_pattern):
         self.states.append((path, pattern, new_pattern))
-        self._i += 1
+        self.state += 1
+
+    def return_state(self):
+        return self.states[self.state]
