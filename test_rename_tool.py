cd41ebfaaad8f31e63f0bc980aae6645a39ca8d1
adam <adam.lepkowski@hotmail.com>
2020-07-02 19:41:54 +0200

    Add test_undo

--- test_rename_tool.py
+++ test_rename_tool.py
@@ -11,3 +11,12 @@ def test_rename(rename, temp_files, tmp_path):
     result = rename.rename(tmp_path, pattern, new_pattern)
     expected = [f'{new_pattern}{i}.txt' for i in range(10)]
     assert sorted(result) == sorted(expected)
+
+
+def test_undo(rename, temp_files, tmp_path):
+    pattern = 'my_file'
+    new_pattern = 'test_file'
+    rename.rename(tmp_path, pattern, new_pattern)
+    result = rename.undo()
+    expected = [f'{pattern}{i}.txt' for i in range(10)]
+    assert sorted(result) == sorted(expected)
