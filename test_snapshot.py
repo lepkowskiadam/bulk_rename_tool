68b3ba700e734da23c74467caf68e5727091c322
adam <adam.lepkowski@hotmail.com>
2020-07-02 13:28:31 +0200

    Add test_redo

--- test_snapshot.py
+++ test_snapshot.py
@@ -44,3 +44,32 @@ def test_undo(snapshot):
     assert path == test_path_1
     assert old == test_pattern_1
     assert new == test_new_pattern_1
+
+
+def test_redo(snapshot):
+    test_path_1 = 'test/path'
+    test_pattern_1 = 'test_pattern'
+    test_new_pattern_1 = 'test_new_pattern'
+    test_path_2 = 'test/path'
+    test_pattern_2 = 'test_pattern_2'
+    test_new_pattern_2 = 'test_new_pattern_2'
+    snapshot.save_state(test_path_1, test_pattern_1, test_new_pattern_1)
+    snapshot.save_state(test_path_2, test_pattern_2, test_new_pattern_2)
+    snapshot.undo()
+    result = snapshot.return_state()
+    path, old, new = result
+    assert path == test_path_1
+    assert old == test_pattern_1
+    assert new == test_new_pattern_1
+    snapshot.redo()
+    result = snapshot.return_state()
+    path, old, new = result
+    assert path == test_path_2
+    assert old == test_pattern_2
+    assert new == test_new_pattern_2
+    snapshot.redo()
+    result = snapshot.return_state()
+    path, old, new = result
+    assert path == test_path_2
+    assert old == test_pattern_2
+    assert new == test_new_pattern_2
