a3a64f71ad3aedbe2b36695e9893decd79e51590
adam <adam.lepkowski@hotmail.com>
2020-07-02 13:04:21 +0200

    Add test_undo

--- test_snapshot.py
+++ test_snapshot.py
@@ -16,3 +16,31 @@ def test_return_state(snapshot):
     result = snapshot.return_state()
     assert type(result) == tuple
     assert len(result) == 3
+
+
+def test_undo(snapshot):
+    test_path_1 = 'test/path'
+    test_pattern_1 = 'test_pattern'
+    test_new_pattern_1 = 'test_new_pattern'
+    test_path_2 = 'test/path'
+    test_pattern_2 = 'test_pattern_2'
+    test_new_pattern_2 = 'test_new_pattern_2'
+    snapshot.save_state(test_path_1, test_pattern_1, test_new_pattern_1)
+    snapshot.save_state(test_path_2, test_pattern_2, test_new_pattern_2)
+    result = snapshot.return_state()
+    path, old, new = result
+    assert path == test_path_2
+    assert old == test_pattern_2
+    assert new == test_new_pattern_2
+    snapshot.undo()
+    result = snapshot.return_state()
+    path, old, new = result
+    assert path == test_path_1
+    assert old == test_pattern_1
+    assert new == test_new_pattern_1
+    snapshot.undo()
+    result = snapshot.return_state()
+    path, old, new = result
+    assert path == test_path_1
+    assert old == test_pattern_1
+    assert new == test_new_pattern_1
