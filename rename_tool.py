83d9bed59447f81cb84261cac1333898cb9e2a70
adam <adam.lepkowski@hotmail.com>
2020-07-02 19:41:39 +0200

    Add undo

--- rename_tool.py
+++ rename_tool.py
@@ -1,9 +1,11 @@
+from snapshot import Snapshot
 import os
 
 
 class RenameTool:
     def __init__(self, matcher):
         self.matcher = matcher
+        self.snapshot = Snapshot()
 
     def rename(self, path, pattern, new_pattern):
         if os.path.exists(path):
@@ -13,4 +15,10 @@ class RenameTool:
                 new = file.replace(pattern, new_pattern)
                 updated.append(new)
                 os.rename(dst=os.path.join(path, new), src=os.path.join(path, file))
+            self.snapshot.save_state(path, pattern, new_pattern)
             return updated
+
+    def undo(self):
+        self.snapshot.undo()
+        path, new_pattern, old_pattern = self.snapshot.return_state()
+        return self.rename(path, old_pattern, new_pattern)
