2bc2b6d89c505a781dcdf4f90484c429b552749e
adam <adam.lepkowski@hotmail.com>
2020-07-04 10:41:51 +0200

    Add main program func

---
+++ main.py
@@ -0,0 +1,26 @@
+from rename_tool import RenameTool
+from user_msg import UserMessage
+
+
+def main():
+    rename_tool = RenameTool()
+    msg = UserMessage()
+    command = ''
+    while command != 'q':
+        command = msg.prompt()
+        if command.lower() == 'rename':
+            try:
+                path, pattern, new_pattern = msg.rename()
+                rename_tool.rename(path, pattern, new_pattern)
+            except TypeError:
+                pass
+        else:
+            try:
+                if getattr(msg, command)():
+                    getattr(rename_tool, command)()
+            except AttributeError:
+                msg.command_not_found(command)
+
+
+if __name__ == '__main__':
+    main()
