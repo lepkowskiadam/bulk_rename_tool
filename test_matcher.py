aff2649b5e59385ee9f533beec5435924bf2f4e7
adam <adam.lepkowski@hotmail.com>
2020-07-11 22:48:31 +0200

    Add test_match_multiple

--- test_matcher.py
+++ test_matcher.py
@@ -11,3 +11,9 @@ def test_match(matcher, tmp_path, temp_files):
     expected = ['my_file1.txt']
     assert result == expected
 
+
+def test_match_multiple(matcher, tmp_path, multiple_patts):
+    patterns = {'ś': 's', 'ą': 'a', 'ę': 'e'}
+    expected = [(f'file_ąśę{i}.txt', ['ś', 'ą', 'ę']) for i in range(10)]
+    result = matcher.match_multiple(tmp_path, patterns)
+    assert sorted(result) == sorted(expected)
