7c98e8b0af7184eaa1f3f68c2fbdc6c27b2a5270
adam <adam.lepkowski@hotmail.com>
2020-07-11 22:48:13 +0200

    Add match_multiple

--- matcher.py
+++ matcher.py
@@ -13,3 +13,16 @@ class Matcher:
             if mo is not None:
                 matches.append(file)
         return matches
+
+    @staticmethod
+    def match_multiple(path, patterns: dict):
+        matches = []
+        for file in os.listdir(path):
+            hits = []
+            for pattern in patterns:
+                search_pattern = re.compile(r'{}'.format(pattern))
+                mo = search_pattern.search(file)
+                if mo is not None:
+                    hits.append(pattern)
+            matches.append((file, hits))
+        return matches
