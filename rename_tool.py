9b709ceb495d8efbedc6dabeacac5f6265ca7114
adam <adam.lepkowski@hotmail.com>
2020-07-11 21:55:57 +0200

    Refactor undo/redo/rename/rename_full

--- rename_tool.py
+++ rename_tool.py
@@ -16,18 +16,18 @@ class RenameTool:
             new = file.replace(pattern, new_pattern)[:-len(ext)] + ext
             updated.append(new)
             os.rename(dst=os.path.join(path, new), src=os.path.join(path, file))
-        self.snapshot.save_state(path, pattern, new_pattern)
+        self.snapshot.save_state(path, pattern, new_pattern, 'rename')
         return updated
 
     def undo(self):
         self.snapshot.undo()
-        path, new_pattern, old_pattern = self.snapshot.return_state()
-        return self.rename(path, old_pattern, new_pattern)
+        path, new_pattern, old_pattern, func = self.snapshot.return_state()
+        return getattr(self, func)(path, old_pattern, new_pattern)
 
     def redo(self):
         self.snapshot.redo()
-        path, new_pattern, old_pattern = self.snapshot.return_state()
-        return self.rename(path, old_pattern, new_pattern)
+        path, new_pattern, old_pattern, func = self.snapshot.return_state()
+        return getattr(self, func)(path, old_pattern, new_pattern)
 
     def display_changes(self, path, pattern, new_pattern):
         to_update = self.matcher.match(path, pattern)
@@ -46,5 +46,5 @@ class RenameTool:
             new = new_pattern + f'_{num}' + ext
             updated.append(new)
             os.rename(dst=os.path.join(path, new), src=os.path.join(path, file))
-        self.snapshot.save_state(path, pattern, new_pattern)
+        self.snapshot.save_state(path, pattern, new_pattern, 'rename_full')
         return updated
