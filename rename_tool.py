adf7e886e8614f63d17b361b13c70b69f0e0bcad
adam <adam.lepkowski@hotmail.com>
2020-07-14 20:23:34 +0200

    Refactor renaming methods
    now suited to store lists of file names in snapshots

--- rename_tool.py
+++ rename_tool.py
@@ -9,50 +9,54 @@ class RenameTool:
         self.snapshot = Snapshot()
 
     def rename(self, path, pattern, new_pattern):
-        to_update = self.matcher.match(path, pattern)
-        updated = []
-        for file in to_update:
+        old_file_names = self.matcher.match(path, pattern)
+        new_file_names = []
+        for file in old_file_names:
             ext = '.' + file.rsplit('.')[-1]
             new = file.replace(pattern, new_pattern)[:-len(ext)] + ext
-            updated.append(new)
+            new_file_names.append(new)
             os.rename(dst=os.path.join(path, new), src=os.path.join(path, file))
-        self.snapshot.save_state(path, pattern, new_pattern, 'rename')
-        return updated
+        self.snapshot.save_state(path, old_file_names, new_file_names)
+        return new_file_names
 
     def undo(self):
         self.snapshot.undo()
-        path, new_pattern, old_pattern, func = self.snapshot.return_state()
-        return getattr(self, func)(path, old_pattern, new_pattern)
+        path, new_file_names, old_file_names = self.snapshot.return_state()
+        for new, old in zip(new_file_names, old_file_names):
+            os.rename(dst=os.path.join(path, new), src=os.path.join(path, old))
+        return new_file_names
 
     def redo(self):
         self.snapshot.redo()
-        path, new_pattern, old_pattern, func = self.snapshot.return_state()
-        return getattr(self, func)(path, old_pattern, new_pattern)
+        path, old_file_names, new_file_names = self.snapshot.return_state()
+        for new, old in zip(new_file_names, old_file_names):
+            os.rename(dst=os.path.join(path, new), src=os.path.join(path, old))
+        return new_file_names
 
     def display_changes(self, path, pattern, new_pattern):
-        to_update = self.matcher.match(path, pattern)
-        updated = []
-        for file in to_update:
+        old_file_names = self.matcher.match(path, pattern)
+        new_file_names = []
+        for file in old_file_names:
             ext = '.' + file.rsplit('.')[-1]
             new = file.replace(pattern, new_pattern)[:-len(ext)] + ext
-            updated.append(new)
-        return zip(to_update, updated)
+            new_file_names.append(new)
+        return zip(old_file_names, new_file_names)
 
     def rename_full(self, path, pattern, new_pattern):
-        to_update = self.matcher.match(path, pattern)
-        updated = []
-        for num, file in enumerate(to_update):
+        old_file_names = self.matcher.match(path, pattern)
+        new_file_names = []
+        for num, file in enumerate(old_file_names):
             ext = '.' + file.rsplit('.')[-1]
             new = new_pattern + f'_{num}' + ext
-            updated.append(new)
+            new_file_names.append(new)
             os.rename(dst=os.path.join(path, new), src=os.path.join(path, file))
-        self.snapshot.save_state(path, pattern, new_pattern, 'rename_full')
-        return updated
+        self.snapshot.save_state(path, old_file_names, new_file_names)
+        return new_file_names
 
     def rename_multiple(self, path, patterns: dict):
-        to_update = self.matcher.match_multiple(path, patterns)
+        old_file_names = self.matcher.match_multiple(path, patterns)
         updated = []
-        for file, hits in to_update:
+        for file, hits in old_file_names:
             ext = '.' + file.rsplit('.')[-1]
             new_file = file
             for hit in hits:
