1f624162f82a5c834169ccc82e651e3b701820ac
adam <adam.lepkowski@hotmail.com>
2020-07-15 22:18:47 +0200

    Refactor rename method
    rename was removed and replaced by replace method

--- user_msg.py
+++ user_msg.py
@@ -4,12 +4,20 @@ import os
 class UserMessage:
 
     @staticmethod
-    def rename():
+    def replace():
         path = input('Enter directory path: ')
+        old_new = dict()
         if os.path.exists(path):
-            pattern = input('Enter pattern to be replaced: ')
-            new_pattern = input('Enter new pattern: ')
-            return path, pattern, new_pattern
+            patterns = input('Enter patterns to be replaced (separate them using ","): ')
+            new_patterns = input('Enter new patterns in the same order: ')
+            patterns = patterns.split(', ')
+            new_patterns = new_patterns.split(', ')
+            if len(patterns) == len(new_patterns):
+                for old, new in zip(patterns, new_patterns):
+                    old_new[old] = new
+                return path, old_new
+            else:
+                print('The number of patterns don\'t match')
         print('Path does not exist')
 
     @staticmethod
@@ -34,7 +42,7 @@ class UserMessage:
     @staticmethod
     def prompt():
         print('Enter "q" to quit')
-        command = input('Available options: rename/undo/redo: ')
+        command = input('Available options: replace/undo/redo: ')
         return command.lower()
 
     @staticmethod
